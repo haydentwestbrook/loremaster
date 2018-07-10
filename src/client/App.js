1c4aa69d1daea3ec35f6aa7cec94e508d6da937d
Hayden Westbrook <haydentwestbrook@gmail.com>
2018-07-09 17:47:18 -0700

    Setting up react project

---
+++ src/client/App.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import Main from './components/Main/Main';
+import Loading from './components/Loading/Loading';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+  }
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(data => this.setState({ loaded: true }));
+  }
+
+  render() {
+    const { loaded } = this.state;
+    return (
+      <div>
+        {loaded ? <Main /> : null}
+        <Loading loaded={loaded} />
+      </div>
+    );
+  }
+}
---
+++ src/client/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+  const click = () => {};
+
+  return (
+    <div className="main">
+      <h1>React App</h1>
+      <button onClick={click}>Click</button>
+    </div>
+  );
+};
+
+export default Main;
