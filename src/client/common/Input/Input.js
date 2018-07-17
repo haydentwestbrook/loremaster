b3d82a9424a03a3b19494526eb51de06e1e4b414
Hayden Westbrook <haydentwestbrook@gmail.com>
2018-07-16 20:52:21 -0700

    ddign API support

--- src/client/common/Input/Input.js
+++ src/client/common/Input/Input.js
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Input = props => {
-  const { value, write, label, onChange } = props;
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false
+    };
 
-  return (
-    <div className="form-group">
-      <label>{label}</label>
-      <input
-        disabled={!write}
-        className="input-block"
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSuccess = this.handleSuccess.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  handleChange = e => {
+    const { onChange, validation } = this.props;
+    if (!validation) onChange(e);
+    else {
+      const v = e.target.value;
+      if (Number(v) && validation === 'number') this.handleSuccess(e);
+      else if (Boolean(v) && validation === 'boolean') this.handleSuccess(e);
+      else if (typeof validation === 'function' && validation(v))
+        this.handleSuccess(e);
+      else this.handleError(e);
+    }
+  };
+
+  handleSuccess = e => {
+    const { onChange } = this.props;
+    this.setState({ error: false });
+    onChange(e);
+  };
+
+  handleError = e => {
+    this.setState({ error: true });
+  };
+
+  render() {
+    const { value, write, label, validation } = this.props;
+    const { error } = this.state;
+    return (
+      <div className="form-group">
+        <label>{label}</label>
+        <input
+          disabled={!write}
+          className={'input-block' + (error ? ' alert alert-danger' : '')}
+          type="text"
+          value={error ? '' : value}
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        {error ? (
+          <label className="input__error">
+            value must be a {String(validation)}
+          </label>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Input;
--- src/client/components/Main/Main.js
+++ src/client/components/Main/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Provider } from 'unstated';
-import Character from '../Character/Character';
+import CharacterSheet from '../CharacterSheet/CharacterSheet';
+import Lookup from '../Lookup/Lookup';
+import CharacterContainer from '../containers/CharacterContainer/CharacterContainer';
+import FiveEContainer from '../containers/FiveEContainer/FiveEContainer';
 
 const Main = () => {
   return (
     <div className="main">
       <h1>Lore Master</h1>
-      <Provider>
-        <Character />
+      <Provider inject={[CharacterContainer, FiveEContainer]}>
+        <CharacterSheet />
+        <Lookup />
       </Provider>
     </div>
   );
